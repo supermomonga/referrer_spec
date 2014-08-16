c81c541f8ae5d91c349a6d1e40948b5c46d2eb17
supermomonga <hi@supermomonga.com>
2014-08-16 23:28:35 +0900

    basic views

--- app.rb
+++ app.rb
@@ -2,11 +2,31 @@
 
 require 'bundler'
 Bundler.require
-
 require 'sinatra/reloader' if development?
 
 
+
+before do
+  @site_title = 'Awesome benri site'
+end
+
+
+
+# Pages
+
 get '/' do
-  content_type :text
-  'Hello Heroku'
+  @page_title = 'Index'
+  slim :index
+end
+
+
+
+# Assets
+
+get '/assets/application.css' do
+  scss :'../assets/css/application'
+end
+
+get '/assets/application.js' do
+  coffee :'../assets/js/application'
 end
