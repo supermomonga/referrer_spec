e333e4c8d8fb17d0f4f569baad492a84975035ed
supermomonga <hi@supermomonga.com>
2014-08-17 18:49:47 +0900

    add some routing samples

--- app.rb
+++ app.rb
@@ -12,11 +12,31 @@ end
 
 
 
-# Pages
+# Page samples
 
 get '/' do
   @page_title = 'Index'
   slim :index
+end
+
+get '/hello/:name' do
+  "Hello #{params[:name]}!"
+end
+
+get '/hello/:name/:age' do |name, age|
+  "You are #{name}, age is #{age}."
+end
+
+get '/hello/*/*' do |name, age|
+  "You are #{name}, age is #{age}."
+end
+
+get '/posts.?:format?' do
+  # It matches "/posts", "/posts.json" and "/posts.xml" and other extensions.
+end
+
+post '/hi' do
+  "#{params[:post_field]}"
 end
 
 
