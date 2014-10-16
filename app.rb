6d4ed0761394239b42a8269418fe74019d302cde
supermomonga <hi@supermomonga.com>
2014-10-17 07:46:00 +0900

    diable log

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ require 'bundler'
 Bundler.require
 require 'pp'
 
+module Rack
+  class CommonLogger
+    def call(env)
+      # do nothing
+      @app.call(env)
+    end
+  end
+end
+
 class App < Sinatra::Base
 
   def initialize *args
@@ -12,12 +21,20 @@ class App < Sinatra::Base
     super
   end
 
+  before do
+  end
+
   configure :development do
     require 'sinatra/reloader'
     register Sinatra::Reloader
   end
 
   get '/' do
+    logger.info "GGG"
+    puts "\n\n"
+    puts "↓" * 60
+    puts "↓" * 60
+    puts "\n\n"
     slim :index
   end
 
@@ -28,22 +45,29 @@ class App < Sinatra::Base
       referrer: request.referrer,
       ip_address: request.ip,
     }
-    puts '-' * 20
-    puts @browser[:page].upcase
-    pp @browser
-    redirect to('/inside_iframe_redirected')
+    print_brower @browser
+    slim :inside_iframe
   end
 
-  get '/inside_iframe_redirected' do
+  get '/inside_iframe_nested' do
     @browser = {
-      page: 'inside_iframe_redirected',
+      page: 'inside_iframe_nested',
       user_agent: request.user_agent,
       referrer: request.referrer,
       ip_address: request.ip,
     }
-    puts '-' * 20
-    puts @browser[:page].upcase
-    pp @browser
+    print_brower @browser
+    redirect to('/inside_iframe_nested_redirected')
+  end
+
+  get '/inside_iframe_nested_redirected' do
+    @browser = {
+      page: 'inside_iframe_nested_redirected',
+      user_agent: request.user_agent,
+      referrer: request.referrer,
+      ip_address: request.ip,
+    }
+    print_brower @browser
     'Hello World'
     # slim :browser_information
   end
@@ -54,6 +78,16 @@ class App < Sinatra::Base
 
   get '/assets/application.js' do
     coffee :'../assets/js/application'
+  end
+
+  private
+  def print_brower b
+    puts
+    puts "=" * 60
+    puts b[:page].upcase
+    puts "  => USER AGENT : #{b[:user_agent][0..40]}"
+    puts "  => REFERRER   : #{b[:referrer]}"
+    puts "  => IP ADDRESS : #{b[:ip_address]}"
   end
 
 end
